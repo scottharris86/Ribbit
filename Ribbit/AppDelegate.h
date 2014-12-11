33c8d5ba903a0cf944f81f240a2e517d94bdc329
Scott Harris <scottharris@outlook.com>
2014-12-11 16:31:24 -0500

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by scott harris on 12/11/14.
+//  Copyright (c) 2014 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by scott harris on 12/11/14.
+//  Copyright (c) 2014 scott harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
